6961e893689521007eb6889cd1f8aa497ffc961e
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-07-31 11:07:58 +0700

    First commit

---
+++ crudProject/src/test/java/com/SpringReact/crudProject/CrudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringReact.crudProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
